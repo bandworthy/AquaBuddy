c79c44eee703c32b1eca84940b8ae0b33580b96d
bandworthy <s3240684@student.rmit.edu.au>
2017-03-31 16:37:34 +1000

    Listview basic working with stripped unneed strings

--- AquaBuddy/FileListActivity.cs
+++ AquaBuddy/FileListActivity.cs
@@ -13,37 +13,67 @@ using Android.Widget;
 
 namespace AquaBuddy
 {
+
+    
+
     [Activity(Label = "File List Test")]
-    public class fileListActivity : Activity
+    public class fileListActivity : ListActivity
     {
-       
+
+        string[] fileList;
+        List<string> newlist = new List<string>();
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-           
-            SetContentView(Resource.Layout.FileListLO);
+
+            //since the default listview is built in you dont assign a custom view
+            //SetContentView(Resource.Layout.FileListLO);
 
             // Create your application here
             //string path = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryMusic).ToString();
             try
             {
                 //var musicFiles = Directory.EnumerateFiles(Android.OS.Environment.DirectoryMusic, "*.mp3");
-                var musicFiles = Directory.EnumerateFiles("/storage/emulated/0/Music/","*.mp3");
-                string[] fileList = musicFiles.ToArray();
-            //or do a foreach var
+                var musicFiles = Directory.EnumerateFiles("/storage/emulated/0/Music/", "*.mp3");
+                fileList = musicFiles.ToArray();
 
-                foreach (var b in musicFiles)
+                //or do a foreach var
+                foreach (var b in fileList)
                 {
-                    Toast.MakeText(this, b, ToastLength.Long).Show();
+                    //Toast.MakeText(this, b, ToastLength.Long).Show();
+                    //removing unwanted text from each song name
+                    // adding to new list which will become the display array
+ 
+                    string c = b;
+                    c = b.Replace(".mp3", "");
+                    c = c.Replace("/storage/emulated/0/Music/", "");
+
+                    newlist.Add(c);
                 }
+
+                newlist.ToArray();
+
+
+                ListAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, newlist);
+
+
+
             }
-            catch(DirectoryNotFoundException e)
+            catch (DirectoryNotFoundException e)
             {
                 // no directory
                 Toast.MakeText(this, "No Music Folder", ToastLength.Long).Show();
             }
 
         }
+
+        protected override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            var t = newlist[position];
+            Toast.MakeText(this, t, ToastLength.Short).Show();
+        }
+
+
     }
 }
